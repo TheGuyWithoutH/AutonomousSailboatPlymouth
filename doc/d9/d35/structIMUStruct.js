849e1cf21b1c3288967c41666d24868bb31195e5
Ugo Balducci <51507845+TheGuyWithoutH@users.noreply.github.com>
2022-03-23 19:23:42 +0100

    Delete doc/d9 directory

--- doc/d9/d35/structIMUStruct.js
+++
@@ -1,9 +0,0 @@
-var structIMUStruct =
-[
-    [ "Accelero", "d9/d35/structIMUStruct.html#aec305a17aa069a9b64c191956c388ecb", null ],
-    [ "Gyro", "d9/d35/structIMUStruct.html#af1b286a299c308f5773688d14a21207e", null ],
-    [ "Magneto", "d9/d35/structIMUStruct.html#a5ba27afc9bac350793cac440086b49c7", null ],
-    [ "p_Accelero", "d9/d35/structIMUStruct.html#af56ec42f34753d1d395f7bcf40acfb7b", null ],
-    [ "p_Gyro", "d9/d35/structIMUStruct.html#aa794b8400d368a392fe6c5bf6885d261", null ],
-    [ "p_Magneto", "d9/d35/structIMUStruct.html#a5f902a950fa8fc2bf02def15ec3ef706", null ]
-];
--- doc/d9/db5/classUltrasonic.js
+++
@@ -1,9 +0,0 @@
-var classUltrasonic =
-[
-    [ "Ultrasonic", "d9/db5/classUltrasonic.html#a8f7340329f1e8c3cdbcc23b35199ae72", null ],
-    [ "CalcDistance", "d9/db5/classUltrasonic.html#afd0c3dcb7a92b542cc3f9482f3f7c7b4", null ],
-    [ "timing", "d9/db5/classUltrasonic.html#af7a139ffc84db427e34575ee040d6930", null ],
-    [ "_CM_DIVISOR", "d9/db5/classUltrasonic.html#a74009a7e517c70f69a99057543eadc83", null ],
-    [ "_ECHO_PIN", "d9/db5/classUltrasonic.html#a610a3a85cf12595372edbd42a846a6f2", null ],
-    [ "_TRIG_PIN", "d9/db5/classUltrasonic.html#a8ab3ddf37f0a65a89eed2eece9728b9e", null ]
-];
